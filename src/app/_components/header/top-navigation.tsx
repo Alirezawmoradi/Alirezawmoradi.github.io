c0434402a4778d8aa0d73082c12a30672acccdeb
KMflutter <kmflutterteam@gmail.com>
2023-12-13 20:05:33 +0330

    responsive for devices

--- src/app/_components/header/top-navigation.tsx
+++ src/app/_components/header/top-navigation.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React from "react";
+import React, {useState} from "react";
 import Link from "next/link";
 import {usePathname} from "next/navigation";
 
@@ -17,21 +17,65 @@ const menuItems: NavigationMenuItem[] = [
 ]
 export const TopNavigation: React.FC = () => {
     const pathname = usePathname();
+    const [toggle, setToggle] = useState(false);
     return (
-        <ul className='list-none hidden sm:flex flex-row gap-14 mt-2'>
-            {
-                menuItems.map((item) => {
-                    const isActive = pathname === item.href;
-                    return (
-                        <li key={item.href} className={`navigation-${item.href}`}>
-                            <Link href={item.href}
-                                  className={`hover:text-taupe text-[21px] font-medium mova uppercase tracking-[3px] cursor-pointer nav-links ${isActive ? 'text-french' : 'text-eerieBlack'}`}>
-                                {item.title}
-                            </Link>
-                        </li>
+        <>
+            <ul className='list-none hidden sm:flex flex-row gap-14 mt-2'>
+                {
+                    menuItems.map((item) => {
+                        const isActive = pathname === item.href;
+                        return (
+                            <li key={item.href} className={`navigation-${item.href}`}>
+                                <Link href={item.href}
+                                      className={`hover:text-taupe text-[21px] font-medium mova uppercase tracking-[3px] cursor-pointer nav-links ${isActive ? 'text-french' : 'text-eerieBlack'}`}>
+                                    {item.title}
+                                </Link>
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+            <div className='sm:hidden flex flex-1 w-screen justify-end items-center'>
+                {
+                    toggle ? (
+                        <div
+                            className={`p-10 bg-flashWhite opacity-[0.98] absolute top-0 left-0 w-screen h-[100vh] z-10 menu${toggle ? 'menu-open' : 'menu-close'}`}>
+                            <div className='flex justify-end'>
+                                <img
+                                    src={'/images/icons/close.png'}
+                                    alt='close'
+                                    className='w-[22px] h-[22px] object-contain cursor-pointer'
+                                    onClick={() => setToggle(!toggle)}
+                                />
+                            </div>
+                            <ul className="list-none flex flex-col -gap-[1rem] items-start justify-end mt-[10rem] -ml-[35px]">
+                                {
+                                    menuItems.map((item) => {
+                                        const isActive = pathname === item.href;
+                                        return (
+                                            <li key={item.href} className={`navigation-${item.href}`}>
+                                                <Link href={item.href}
+                                                      className={`text-[55px] font-bold arenq uppercase tracking-[1px] cursor-pointer ${isActive ? 'text-french' : 'text-eerieBlack'}`}
+                                                      onClick={() => setToggle(!toggle)}
+                                                >
+                                                    {item.title}
+                                                </Link>
+                                            </li>
+                                        )
+                                    })
+                                }
+                            </ul>
+                        </div>
+                    ) : (
+                        <img
+                            src={'images/icons/menu.png'}
+                            alt="menu"
+                            className="w-[34px] h-[34px] object-contain cursor-pointer"
+                            onClick={() => setToggle(!toggle)}
+                        />
                     )
-                })
-            }
-        </ul>
+                }
+            </div>
+        </>
     )
 }
